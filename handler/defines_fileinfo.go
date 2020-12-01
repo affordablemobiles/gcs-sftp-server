2968b66b1e5556a552e9b883781bc33796dabe51
Samuel Melrose <sam.melrose@a1comms.com>
2020-12-01 14:46:51 +0000

    directory listing fixed

--- handler/defines_fileinfo.go
+++ handler/defines_fileinfo.go
@@ -13,11 +13,11 @@ type SyntheticFileInfo struct {
 }
 
 func (f *SyntheticFileInfo) Name() string { // base name of the file
-	if f.objAttr.Prefix == "" {
-		return f.objAttr.Name[len(f.prefix):]
-	} else {
+	if f.objAttr.Prefix != "" {
 		return f.objAttr.Prefix[len(f.prefix) : len(f.objAttr.Prefix)-1]
 	}
+
+	return f.objAttr.Name[len(f.prefix):]
 }
 
 func (f *SyntheticFileInfo) Size() int64 { // length in bytes for regular files; system-dependent for others
@@ -25,23 +25,31 @@ func (f *SyntheticFileInfo) Size() int64 { // length in bytes for regular files;
 }
 
 func (f *SyntheticFileInfo) Mode() os.FileMode { // file mode bits
-	if f.objAttr.Prefix == "" {
-		return 0777
-	} else {
+	if f.IsDir() {
 		return os.ModeDir | 0777
 	}
+
+	return 0777
 }
 
 func (f *SyntheticFileInfo) ModTime() time.Time { // modification time
-	if f.objAttr.Prefix == "" {
-		return f.objAttr.Updated
-	} else {
+	if f.IsDir() {
 		return time.Now()
 	}
+
+	return f.objAttr.Updated
 }
 
 func (f *SyntheticFileInfo) IsDir() bool { // abbreviation for Mode().IsDir()
-	return f.objAttr.Prefix == ""
+	if f.objAttr.Prefix != "" {
+		return true
+	} else if len(f.objAttr.Name) > 0 {
+		if f.objAttr.Name[len(f.objAttr.Name)-1:] == "/" && f.objAttr.Size == 0 {
+			return true
+		}
+	}
+
+	return false
 }
 
 func (f *SyntheticFileInfo) Sys() interface{} { // underlying data source (can return nil)
